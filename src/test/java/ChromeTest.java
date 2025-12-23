5d6f5a3632a43866eea991168c0776b98a2693b5
KevinGr <kgomezr2@unicartagena.edu.co>
2025-12-23 16:19:33 -0500

    feat: ChromeTest is a class for Test the WebDriver Manager

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class ChromeTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUpChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    @Test
+    public void testChrome() {
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Google");
+    }
+
 }
